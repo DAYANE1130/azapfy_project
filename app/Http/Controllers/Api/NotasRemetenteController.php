2a2f57d406796a4c2a8bfc2be5c9ad4aa33b7126
Dayane <dayanebmartins@gmail.com>
2023-11-08 18:41:09 -0300

    eclui arquivos desnecessarios

--- app/Http/Controllers/Api/NotasRemetenteController.php
+++ app/Http/Controllers/Api/NotasRemetenteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\NotasRemetenteService;
-
+//use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class NotasRemetenteController extends Controller
 {
